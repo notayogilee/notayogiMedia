9a518233e056b4b0396065b904061425795b455e
notayogilee <notayogilee@gmail.com>
2024-11-13 15:37:06 -0800

    Change fetch posts chain.

--- app.js
+++ app.js
@@ -7,28 +7,29 @@ const createImageUrl = (source) => {
   return builder.image(source);
 };
 
+const generatePosts = (posts) => {
+  const postSection = document.getElementById("postSection");
+
+  posts.forEach(({ title, subtitle, image }) => {
+    const imageUrl = createImageUrl(image).url();
+    const postElement = `<div>
+                          <h1>${title}</h1>
+                          <h2>${subtitle}</h2>
+                          <img class='post-img' src=${imageUrl} alt=${title} />
+                        </div>`;
+    postSection.insertAdjacentHTML("beforeend", postElement);
+  });
+};
+
 const getAllPosts = async () => {
   const query = '*[_type == "post"]';
   try {
     const data = await client.fetch(query);
+    generatePosts(data);
     return data;
   } catch (error) {
     console.error("Query failed: ", error);
   }
 };
 
-const posts = await getAllPosts();
-
-const postSection = document.getElementById("postSection");
-
-posts.forEach(({ title, subtitle, image }) => {
-  const imageUrl = createImageUrl(image).url();
-  const postElement = `<div>
-                        <h1>${title}</h1>
-                        <h2>${subtitle}</h2>
-                        <img class='post-img' src=${imageUrl} alt=${title} />
-                      </div>`;
-  postSection.insertAdjacentHTML("beforeend", postElement);
-});
-
-console.log(posts);
+getAllPosts();
